edd95bd1d23374f51194a6b7e99c22d48ca1f022
Shuai <bfshm@qq.com>
2020-09-14 11:15:04 +0800

    init

---
+++ MyNeoPlugins/MyNeoPlugins/MemPoolPlugin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Neo.IO.Json;
+using Neo.Ledger;
+using Neo.Network.P2P.Payloads;
+using Neo.Plugins;
+
+namespace MyNeoPlugins
+{
+    public class MemPoolPlugin : Plugin, IRpcPlugin
+
+    {
+        public override void Configure()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void PreProcess(HttpContext context, string method, JArray _params)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public JObject OnProcess(HttpContext context, string method, JArray _params)
+        {
+            if (method == "getMemPoolTransactions")
+            {
+                bool shouldGetUnverified = _params.Count >= 1 && _params[0].AsBoolean();
+                if (!shouldGetUnverified)
+                    return new JArray(Blockchain.Singleton.MemPool.GetVerifiedTransactions().Select(tx => new TransactionItem(tx)).OrderByDescending(t => t.TotalFee).Select(p =>
+                        p.ToJObject()));
+
+                JObject json = new JObject();
+                json["height"] = Blockchain.Singleton.Height;
+                Blockchain.Singleton.MemPool.GetVerifiedAndUnverifiedTransactions(
+                    out IEnumerable<Transaction> verifiedTransactions,
+                    out IEnumerable<Transaction> unverifiedTransactions);
+                json["verified"] = new JArray(verifiedTransactions.Select(tx => new TransactionItem(tx)).OrderByDescending(t => t.TotalFee).Select(p =>
+                    p.ToJObject()));
+                json["unverified"] = new JArray(unverifiedTransactions.Select(tx => new TransactionItem(tx)).OrderByDescending(t => t.TotalFee).Select(p =>
+                    p.ToJObject()));
+                return json;
+            }
+
+            return null;
+        }
+
+        public void PostProcess(HttpContext context, string method, JArray _params, JObject result)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyNeoPlugins/MyNeoPlugins/TransactionItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo;
+using Neo.IO.Json;
+using Neo.Network.P2P.Payloads;
+
+namespace MyNeoPlugins
+{
+    public class TransactionItem
+    {
+        public TransactionItem(Transaction tx)
+        {
+            TxId = tx.Hash;
+            SystemFee = tx.SystemFee;
+            NetworkFee = tx.NetworkFee;
+            TotalFee = tx.SystemFee + tx.NetworkFee;
+        }
+        public UInt256 TxId { get; set; }
+
+        public Fixed8 SystemFee { get; set; }
+        public Fixed8 NetworkFee { get; set; }
+
+        public Fixed8 TotalFee { get; set; }
+
+
+        public JObject ToJObject()
+        {
+            var o = new JObject();
+            o["txid"] = TxId.ToString();
+            o["systemFee"] = SystemFee.ToString();
+            o["networkFee"] = NetworkFee.ToString();
+            o["fee"] = TotalFee.ToString();
+            return o;
+        }
+    }
+}
